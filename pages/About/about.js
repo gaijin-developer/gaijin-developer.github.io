6c8ca995617d00cf3d0dd3e3ccab4231ce7946ed
Frank Entsie <frank.entsie301@gmail.com>
2023-04-29 16:54:27 +0900

    about page updated

--- pages/About/about.js
+++ pages/About/about.js
@@ -1,8 +1,6 @@
 const menuButton = document.querySelectorAll('.menuContainer')[0];
 const nav = document.querySelector('nav');
 
-console.log(menuButton);
-console.log(nav);
 
 menuButton.addEventListener("click", function (){
     nav.classList.toggle("active")
